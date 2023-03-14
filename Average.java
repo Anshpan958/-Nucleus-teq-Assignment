3b993f2ef66099e810dd44cc2feac7ec5a9ab01d
Ansh Pandey <anshpandey958@gmail.com>
2023-03-14 19:53:16 +0530

    Average ass done 1

--- Average.java
+++ Average.java
@@ -10,10 +10,9 @@ public class Average {
         System.out.println("Enter 3 number to take out its average:");
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
-        int n3 = sc.nextInt();
-        int ave=0;
-        System.out.println("Average = "+(n1+n2+n3)/3);
+        int n4 = sc.nextInt();
+        int ave = 0;
+        System.out.println("Average = " + (n1 + n2 + n3) / 3);
 
     }
-
 }
