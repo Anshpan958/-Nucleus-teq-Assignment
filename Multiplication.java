ddbe8d684c6b30b9594ab094983381f0bbdbb8b0
Ansh Pandey <anshpandey958@gmail.com>
2023-03-16 12:29:21 +0530

    new beginning

---
+++ Multiplication.java
@@ -0,0 +1,19 @@
+package LearningGIT;
+import java.util.*;
+
+public class Multiplication {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 2 numbers for multiplication.");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a*b;
+        System.out.println("Multiplication of two no is ");
+
+        System.out.println(c);
+
+    }
+
+}
