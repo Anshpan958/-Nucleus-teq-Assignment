e08ababb03a8ac6f9ae81a4a983154ed0cfcca44
Ansh Pandey <anshpandey958@gmail.com>
2023-04-07 13:56:46 +0530

    Frequency

---
+++ week6/Frequency_Map.java
@@ -0,0 +1,29 @@
+package week6;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Frequency_Map {
+
+    static void count(String str){
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        char[] arr = str.toCharArray();
+        for (char c : arr){
+            if (map.containsKey(c)){
+                map.put(c, map.get(c) + 1);
+            }else{
+                map.put(c, 1);
+            }
+        }
+        System.out.println("Character    Frequency");
+        System.out.println("---------    ---------");
+        for (Map.Entry entry : map.entrySet()){
+
+            System.out.println("   "+entry.getKey() + "  ---------> " + entry.getValue());
+        }
+
+    }
+    public static void main(String[] args) {
+        count("fsdjflk234j435kdjhs");
+    }
+
+}
